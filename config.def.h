75a35b7dcce527c98afd4ab7cedfe7fbfcc674da
Pierre Chevalier <pierrechevalier83@gmail.com>
2016-05-21 21:09:42 +0100

    Larger text for my HiDPI screen
    Also, make the font match my awesome setup

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
+static const char font[]        = "Tamsyn:size=16";
 static const char* normbgcolor  = "#073642";
 static const char* normfgcolor  = "#eee8d5";
 static const char* selbgcolor   = "#268bd2";
